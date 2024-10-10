13e963e708e07596afe138cb5e061398b9e460a5
Scott Turro <turroscott@gmail.com>
2024-10-10 14:08:27 -0500

    Final edits

--- src/main.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from datetime import datetime
-
-from database.database import Database
-from database.models import Brand, Receipt, Transaction, User
-from sqlalchemy import func, and_
-
-db = Database()
-
